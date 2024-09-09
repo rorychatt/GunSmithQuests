5725613fc7162aa4cf6adbb5bbc812446c748173
rorychatt <pavel.rinne@gmail.com>
2024-09-09 12:39:48 +0200

    Feat: daisyui setup

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -3,9 +3,14 @@ import './App.css'
 function App() {
 
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <button className="btn">
+                Click me
+            </button>
+        </>
     )
 }
 
