b1d08e7b61f91eb12450e6bf1c3f012dadb66d96
rorychatt <pavel.rinne@gmail.com>
2024-09-09 12:35:08 +0200

    Feat: tailwind setup

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,11 +2,11 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-     
-    </>
-  )
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
 }
 
 export default App
