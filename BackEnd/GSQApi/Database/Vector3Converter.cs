48f38a81b5d098f113194663f39c0e7abd9bada2
rorychatt <pavel.rinne@gmail.com>
2024-09-10 12:26:02 +0200

    feat: Vector3Converter.cs for ef core

---
+++ BackEnd/GSQApi/Database/Vector3Converter.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace GSQApi.Database;
+
+public class Vector3Converter() : ValueConverter<Vector3, string>(v => ConvertToString(v),
+    v => ConvertToVector3(v))
+{
+    private static string ConvertToString(Vector3 vector)
+    {
+        return $"{vector.X},{vector.Y},{vector.Z}";
+    }
+
+    private static Vector3 ConvertToVector3(string vectorString)
+    {
+        var parts = vectorString.Split(',');
+        return new Vector3(
+            float.Parse(parts[0]),
+            float.Parse(parts[1]),
+            float.Parse(parts[2])
+        );
+    }
+}
