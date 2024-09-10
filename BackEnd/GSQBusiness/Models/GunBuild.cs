575fd4da452caf606100f5898912463446d57b6b
rorychatt <pavel.rinne@gmail.com>
2024-09-10 09:16:47 +0200

    feat: general classes

---
+++ BackEnd/GSQBusiness/Models/GunBuild.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using GSQBusiness.Contracts;
+
+namespace GSQBusiness.Models;
+
+public class GunBuild : IPositionable, IDescribable
+
+{
+    public Vector3 Position { get; set; } = new Vector3(0, 0, 0);
+    public Vector3 EulerAngles { get; set; } = new Vector3(0, 0, 0);
+    public List<GunPart> GunParts { get; set; } = [];
+    
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+}
---
+++ BackEnd/GSQBusiness/Models/GunPart.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using GSQBusiness.Contracts;
+
+namespace GSQBusiness.Models;
+
+public class GunPart : IPositionable, IDescribable
+{
+    public Vector3 Position { get; set; } = new Vector3(0, 0, 0);
+    public Vector3 EulerAngles { get; set; } = new Vector3(0, 0, 0);
+
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+}
