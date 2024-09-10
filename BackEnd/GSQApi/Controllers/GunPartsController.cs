70b28c048e181159ca4cdcb892298dcf8ced8bea
rorychatt <pavel.rinne@gmail.com>
2024-09-10 09:25:28 +0200

    Feat: get gun part

--- BackEnd/GSQApi/Controllers/GunPartsController.cs
+++ BackEnd/GSQApi/Controllers/GunPartsController.cs
@@ -1,6 +1,24 @@
-﻿namespace GSQApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
 
-public class GunPartsController
+namespace GSQApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GunPartsController(IConfiguration configuration) : ControllerBase
 {
+    private readonly string _folderPath =
+        Path.Combine(AppContext.BaseDirectory, configuration["FileStorage:FolderPath"]!);
     
+    [HttpGet("{filename}")]
+    public IActionResult GetGunPart(string filename)
+    {
+        var filePath = Path.Combine(_folderPath, filename);
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+        
+        var fileBytes = System.IO.File.ReadAllBytes(filePath);
+        return File(fileBytes, "application/octet-stream");
+    }
 }
