90db3be7ac982523aabc7b81f8f938556b8a607c
rorychatt <pavel.rinne@gmail.com>
2024-09-10 12:27:17 +0200

    feat: gunpart ef core support

--- BackEnd/GSQBusiness/Models/GunPart.cs
+++ BackEnd/GSQBusiness/Models/GunPart.cs
@@ -1,13 +1,19 @@
-﻿using System.Numerics;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 using GSQBusiness.Contracts;
 
 namespace GSQBusiness.Models;
 
-public class GunPart : IPositionable, IDescribable
+public class GunPart : IDescribable, IPositionable
 {
-    public Vector3 Position { get; set; } = new Vector3(0, 0, 0);
-    public Vector3 EulerAngles { get; set; } = new Vector3(0, 0, 0);
+    [Key]
+    public int Id { get; set; }
+    public Guid Guid { get; set; } = new();
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string ContentType { get; set; } = null!;
+    public byte[] Content { get; set; } = null!;
 
-    public required string Name { get; set; }
-    public required string Description { get; set; }
+    public Vector3 Position { get; set; } = new(0, 0, 0);
+    public Vector3 EulerAngles { get; set; } = new(0, 0, 0);
 }
