7020be6ae5c75b1f99291c1203dff8e682aa2597
rorychatt <pavel.rinne@gmail.com>
2024-09-10 16:15:54 +0200

    feat: aside template

---
+++ FrontEnd/src/components/Aside.tsx
@@ -0,0 +1,10 @@
+export function Aside() {
+    return (
+        <aside
+            className="flex flex-col items-center justify-center text-center
+             border-2 border-gray-300 rounded-lg p-2">
+            <h2>Aside Content</h2>
+            <p>This is the aside content area</p>
+        </aside>
+    )
+}
