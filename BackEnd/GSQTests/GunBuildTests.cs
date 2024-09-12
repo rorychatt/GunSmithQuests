e39a139bc29fd5c201719ed1fb7d9eac52312a5a
rorychatt <pavel.rinne@gmail.com>
2024-09-12 11:27:07 +0200

    feat: test for removing single part

--- BackEnd/GSQTests/GunBuildTests.cs
+++ BackEnd/GSQTests/GunBuildTests.cs
@@ -16,4 +16,16 @@ public class GunBuildTests
         gunBuild.Attachments.Should().ContainSingle(attachment => attachment.Name == gunPart.Name);
         gunBuild.Attachments[0].Should().BeOfType<Attachment>();
     }
+    
+    [Fact]
+    public void Build_Should_Remove_GunPart()
+    {
+        var gunBuild = new GunBuild();
+        var gunPart = new GunPart { Name = "TestPart" };
+        
+        gunBuild.TryAddPart(gunPart);
+        gunBuild.TryRemovePart(gunPart);
+        
+        gunBuild.Attachments.Should().BeEmpty();
+    }
 }
