2601c115791ff0626d3ff8e85f4ab6b1b358abe8
rorychatt <pavel.rinne@gmail.com>
2024-09-12 11:24:49 +0200

    feat: build tests

---
+++ BackEnd/GSQTests/GunBuildTests.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using GSQBusiness.Models;
+
+namespace GSQTests;
+
+public class GunBuildTests
+{
+    [Fact]
+    public void Build_Should_Add_GunPart()
+    {
+        var gunBuild = new GunBuild();
+        var gunPart = new GunPart { Name = "TestPart" };
+        
+        gunBuild.TryAddPart(gunPart);
+        
+        gunBuild.Attachments.Should().ContainSingle(attachment => attachment.Name == gunPart.Name);
+        gunBuild.Attachments[0].Should().BeOfType<Attachment>();
+    }
+}
