fd2cb8c2003352356928bb95c896f22217194571
rorychatt <pavel.rinne@gmail.com>
2024-09-12 13:13:44 +0200

    feat: gun part component

---
+++ FrontEnd/src/components/GunPartModel.tsx
@@ -0,0 +1,33 @@
+// import { useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { useEffect, useState } from "react";
+import { API_URL } from "../constants.ts";
+import { TransformControls } from "@react-three/drei";
+import {Group, Object3DEventMap} from "three";
+
+interface GunPart {
+    id: string;
+    name: string;
+    description: string;
+}
+
+export function GunPartModel({ part }: { part: GunPart }) {
+    const [obj, setObj] = useState<Group<Object3DEventMap>>();
+
+    useEffect(() => {
+        const loadModel = async () => {
+            const loadedObj = await new OBJLoader().loadAsync(`${API_URL}/GunParts/download/${part.name}.obj`);
+            setObj(loadedObj);
+        };
+
+        loadModel();
+    }, [part]);
+
+    if (!obj) return null;
+
+    return (
+        <TransformControls>
+            <primitive object={obj} />
+        </TransformControls>
+    );
+}
